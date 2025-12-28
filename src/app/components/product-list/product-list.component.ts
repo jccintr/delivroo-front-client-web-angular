37f5482a453c6cbc2ee3e5e78cee0b3496db7bad
Julio Cesar <jccintr@gmail.com>
2025-12-28 16:14:32 -0300

    criado esqueleto de product-list e product-card

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+
+  @Input() products: any[] = [];
+}
