ee334f75501cebc836cab50c4f16043781850d40
Julio Cesar <jccintr@gmail.com>
2025-12-30 01:18:11 -0300

    criado netlify.toml

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
