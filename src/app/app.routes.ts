a8183ef98ac94d08f61bbddb87b417dc7961a59a
Julio Cesar <jccintr@gmail.com>
2025-12-30 00:58:13 -0300

    sei la 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+//import { AppComponent } from './app.component';
 import { StoreComponent } from './store/store.component';
 
 export const routes: Routes = [
    
-    {path: ':store', component: StoreComponent},
+    {path: ':store', component: StoreComponent}
    
 ];
