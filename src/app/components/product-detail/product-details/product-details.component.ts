c6a70019b2dca0e43bf61d1bbc91886ea1fa1acb
Julio Cesar <jccintr@gmail.com>
2026-01-05 17:57:12 -0300

    criado product-add component

--- src/app/components/product-detail/product-details/product-details.component.ts
+++ src/app/components/product-detail/product-details/product-details.component.ts
@@ -6,10 +6,11 @@ import { StoreService } from '../../../services/store.service';
 import { ProductRequiredItemComponent } from "../product-required-item/product-required-item.component";
 import { ProductExtrasComponent } from "../product-extras/product-extras.component";
 import { ProductInstructionsComponent } from "../product-instructions/product-instructions.component";
+import { ProductAddComponent } from "../product-add/product-add.component";
 
 @Component({
   selector: 'app-product-details',
-  imports: [ProductHeaderComponent, ProductDescriptionComponent, ProductRequiredItemComponent, ProductExtrasComponent, ProductInstructionsComponent],
+  imports: [ProductHeaderComponent, ProductDescriptionComponent, ProductRequiredItemComponent, ProductExtrasComponent, ProductInstructionsComponent, ProductAddComponent],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
